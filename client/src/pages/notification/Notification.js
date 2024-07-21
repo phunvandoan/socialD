b84d5e6a651fb96772337dd960e1bfc100df6927
PHUNG VAN DOAN <phungvandoan2k3@gmail.com>
2024-07-21 20:24:51 +0700

    updateNight

--- client/src/pages/notification/Notification.js
+++ client/src/pages/notification/Notification.js
@@ -1,9 +1,28 @@
 import Topbar from "../../components/topbar/Topbar";
 import "./notification.css";
 import ItemNotification from "../../components/itemNotification/ItemNotification";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
 
 function Notification() {
-  let userDataPost = JSON.parse(localStorage.getItem("postFriend"));
+  // let userDataPost = JSON.parse(localStorage.getItem("postFriend"));
+  const { user: currentUser } = useContext(AuthContext);
+  const [userDataPost, setUserDataPost] = useState([]);
+
+  useEffect(() => {
+    const userDataPost = async () => {
+      try {
+        const res = await axios.get(
+          `https://sociald.onrender.com/api/timeline/` + currentUser._id
+        );
+        setUserDataPost(res.data.reverse());
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    userDataPost();
+  }, [currentUser]);
 
   return (
     <>
